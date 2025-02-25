d5e3a70d0084933639b832f1e54c82b0af508529
Rocky Chen <Rocky.Chen@dbca.wa.gov.au>
2025-02-25 13:25:43 +0800

    Remove the lock file if exists

--- slave_poll.py
+++ slave_poll.py
@@ -25,6 +25,7 @@ if __name__ == "__main__":
         #sync file
         #delete lock files if exists
         for f in (".hg/wlock",".hg/store/lock"):
+            logger.error("Try to remove lock file({}) if exists".format(file))
             file = os.path.join(BORG_STATE_HOME,f)
             if os.path.exists(file):
                 try:
